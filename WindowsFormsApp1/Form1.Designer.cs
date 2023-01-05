73c5eabdc6feae8ca03e839db166d89da7b18d3c
Heorhi94 <heorhi.yakabson@gmail.com>
2023-01-05 13:13:33 +0100

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Karaoke
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Karaoke));
+            this.bPlay = new System.Windows.Forms.Button();
+            this.bStop = new System.Windows.Forms.Button();
+            this.bPathText = new System.Windows.Forms.Button();
+            this.bPathMusic = new System.Windows.Forms.Button();
+            this.textMusic = new System.Windows.Forms.RichTextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.songTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bPlay
+            // 
+            this.bPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bPlay.Image = ((System.Drawing.Image)(resources.GetObject("bPlay.Image")));
+            this.bPlay.Location = new System.Drawing.Point(0, 365);
+            this.bPlay.Name = "bPlay";
+            this.bPlay.Size = new System.Drawing.Size(76, 85);
+            this.bPlay.TabIndex = 7;
+            this.bPlay.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.bPlay.UseVisualStyleBackColor = true;
+            this.bPlay.Click += new System.EventHandler(this.bPlay_Click);
+            // 
+            // bStop
+            // 
+            this.bStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bStop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bStop.Image = ((System.Drawing.Image)(resources.GetObject("bStop.Image")));
+            this.bStop.Location = new System.Drawing.Point(76, 365);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(78, 85);
+            this.bStop.TabIndex = 6;
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // bPathText
+            // 
+            this.bPathText.BackColor = System.Drawing.Color.Gray;
+            this.bPathText.Location = new System.Drawing.Point(661, 377);
+            this.bPathText.Name = "bPathText";
+            this.bPathText.Size = new System.Drawing.Size(78, 61);
+            this.bPathText.TabIndex = 3;
+            this.bPathText.Text = "Text";
+            this.bPathText.UseVisualStyleBackColor = false;
+            this.bPathText.Click += new System.EventHandler(this.bPathText_Click);
+            // 
+            // bPathMusic
+            // 
+            this.bPathMusic.BackColor = System.Drawing.Color.Gray;
+            this.bPathMusic.Location = new System.Drawing.Point(577, 377);
+            this.bPathMusic.Name = "bPathMusic";
+            this.bPathMusic.Size = new System.Drawing.Size(78, 61);
+            this.bPathMusic.TabIndex = 4;
+            this.bPathMusic.Text = "Music";
+            this.bPathMusic.UseVisualStyleBackColor = false;
+            this.bPathMusic.Click += new System.EventHandler(this.bPathMusic_Click);
+            // 
+            // textMusic
+            // 
+            this.textMusic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textMusic.ForeColor = System.Drawing.Color.White;
+            this.textMusic.Location = new System.Drawing.Point(0, 0);
+            this.textMusic.Name = "textMusic";
+            this.textMusic.ReadOnly = true;
+            this.textMusic.Size = new System.Drawing.Size(751, 359);
+            this.textMusic.TabIndex = 5;
+            this.textMusic.Text = "";
+            this.textMusic.ZoomFactor = 2F;
+            // 
+            // comboBox
+            // 
+            this.comboBox.BackColor = System.Drawing.Color.Gray;
+            this.comboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Dzisiaj w Betlejem",
+            "Cicha noc",
+            "Przybieżeli do Betlejem"});
+            this.comboBox.Location = new System.Drawing.Point(355, 377);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(194, 24);
+            this.comboBox.TabIndex = 8;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // songTime
+            // 
+            this.songTime.Enabled = true;
+            this.songTime.Interval = 1000;
+            this.songTime.Tick += new System.EventHandler(this.songTime_Tick);
+            // 
+            // Karaoke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(751, 450);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textMusic);
+            this.Controls.Add(this.bPathMusic);
+            this.Controls.Add(this.bPathText);
+            this.Controls.Add(this.bStop);
+            this.Controls.Add(this.bPlay);
+            this.MaximumSize = new System.Drawing.Size(769, 497);
+            this.MinimumSize = new System.Drawing.Size(769, 497);
+            this.Name = "Karaoke";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bPlay;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.Button bPathText;
+        private System.Windows.Forms.Button bPathMusic;
+        private System.Windows.Forms.RichTextBox textMusic;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Timer songTime;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Karaoke : Form
+    {
+        string pathText;
+        int[] time;
+        string textSong;
+        string pathSong;
+        WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+
+        public Karaoke()
+        {
+            InitializeComponent();
+            textMusic.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void bPlay_Click(object sender, EventArgs e)
+        {
+            WMP.URL = pathSong;
+            WMP.controls.play();
+            songTime.Enabled = true;
+            Play();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            WMP.controls.pause();
+            songTime.Enabled = false;
+        }
+
+        private void bPathText_Click(object sender, EventArgs e)
+        {
+            textMusic.Clear();
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                pathText = open.FileName;
+                textMusic.AppendText(ReadText()+"\n");
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox.SelectedIndex == 0)
+            {
+                textMusic.Clear();
+                pathText = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\text\Dzisiaj w Betleem.txt";
+                WMP.URL = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\misic\koleda-dzisiaj-w-betlejem-karaoke.mp3";
+                Play();
+               
+            }
+            if(comboBox.SelectedIndex == 1)
+            {
+                textMusic.Clear();
+                pathText = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\text\Cicha noc.txt";
+                WMP.URL = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\misic\koleda-cicha-noc-karaoke.mp3";
+                Play();
+             
+            }
+            if(comboBox.SelectedIndex == 2)
+            {
+                textMusic.Clear();
+                pathText = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\text\Przybieżeli do Betlejem.txt";
+                WMP.URL = @"D:\StudiesProject\C#\Lab10Karaoke\WindowsFormsApp1\misic\koleda-przybiezeli-do-betlejem-karaoke.mp3";
+                Play();
+               
+            }
+        }
+
+        private void bPathMusic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                pathSong = open.FileName;
+            }
+
+        }
+
+        public string ReadTextAndTime()
+        {
+            using (StreamReader read = new StreamReader(pathText, true))
+            {
+                return read.ReadToEnd();
+            }
+        }
+
+        public string ReadText()
+        {
+            string text = File.ReadAllText(pathText);
+            string pattern = @"\[[0-9]+\]";
+            text = Regex.Replace(text, pattern, "");
+
+            return text;
+        }
+
+        public int[] TimeLine()
+        {
+            textSong = ReadTextAndTime();
+            string[] lines = textSong.Split('\n');
+            time = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].StartsWith("["))
+                {
+                    string timeString = lines[i].Substring(1, 2);
+                    time[i] = int.Parse(timeString);
+                }    
+            }
+            return time;
+        }
+
+        public void TextLine(int currentLine)
+        {
+            textSong = ReadText();
+            string[] lines = textSong.Split('\n');
+            textMusic.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i == currentLine)
+                {
+                    textMusic.SelectionBackColor = Color.Red;
+                }
+                textMusic.AppendText(lines[i] + "\n");    
+            }
+            textMusic.SelectionStart = textMusic.GetFirstCharIndexFromLine(currentLine);
+            textMusic.ScrollToCaret();
+            textMusic.SelectionLength = 0;      
+        }
+        public void Play()
+        {
+            int currentLine = 0;
+            time = TimeLine();
+            TextLine(currentLine);
+            songTime.Interval = time[currentLine] * 1000;
+            songTime.Start();    
+        }
+
+        private void songTime_Tick(object sender, EventArgs e)
+        {
+            songTime.Stop();
+            int currentLine = textMusic.GetLineFromCharIndex(textMusic.SelectionStart);
+            if (currentLine < textMusic.Lines.Length - 1)
+            {       
+                currentLine++;
+                TextLine(currentLine);
+                songTime.Interval = time[currentLine] * 1000;
+                songTime.Start();               
+            }
+            else
+            {
+                WMP.controls.stop();
+            }
+        }
+    }
+}
